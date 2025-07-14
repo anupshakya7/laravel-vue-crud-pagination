6fff8bd3282c31643e34a2e8399a085c5cbd70dd
Dev Krizmatic <dev@krizmatic.com>
2025-07-14 18:23:12 +0545

    Initial Project

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <Post/>
+    </div>
+</template>
+<script>
+import Post from './components/Post.vue';
+
+export default{
+    components:{
+        Post
+    }
+}
+</script>
---
+++ resources/js/components/Post.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="max-w-4xl mx-auto p-4">
+        <h1 class="text-3xl font-bold mb-6">Posts</h1>
+        <form class="mb-6 bg-white p-4 shadow-md rounded-md">
+            <div class="mb-4">
+                <input type="text" placeholder="Title" class="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300"/>
+            </div>
+            <div class="mb-4">
+                <textare placeholder="Title" class="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-300"/>
+            </div>
+        </form>
+    </div>
+</template>
